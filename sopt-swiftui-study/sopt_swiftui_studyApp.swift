646507355dba8002c3e97ff26c56cef16b505f8e
OneTen19 <khy000426@naver.com>
2024-12-28 20:57:58 +0900

    Initial Commit

---
+++ sopt-swiftui-study/sopt_swiftui_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sopt_swiftui_studyApp.swift
+//  sopt-swiftui-study
+//
+//  Created by OneTen on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct sopt_swiftui_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
