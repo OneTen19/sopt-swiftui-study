a82370b814c5cbafb313fa28b2453f06d7fbd715
OneTen19 <khy000426@naver.com>
2024-12-30 22:24:57 +0900

    [feat] 카카오 로그인&로그아웃 구현 및 비동기처리

--- sopt-swiftui-study/KakaoLoginView.swift
+++ sopt-swiftui-study/KakaoLoginView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct KakaoLoginView: View {
     @StateObject var viewModel = KakaoLoginViewModel()
     
+    let loginStateInfo: (Bool) -> String = { isLoggedIn in
+        return isLoggedIn ? "카카오 로그인 됐습니다" : "카카오 로그아웃 됐습니다"
+    }
+    
     var body: some View {
         VStack(spacing: 20) {
-            Text("Hello, world!")
+            Text(loginStateInfo(viewModel.isLoggedIn))
             
             Button("카카오 로그인") {
-                viewModel.kakaoLogin()
+                viewModel.handleKakaoLogin()
             }
             
             Button("카카오 로그아웃"){
--- sopt-swiftui-study/KakaoLoginViewModel.swift
+++ sopt-swiftui-study/KakaoLoginViewModel.swift
@@ -10,44 +10,76 @@ import SwiftUI
 import Combine
 import KakaoSDKUser
 
+@MainActor
 class KakaoLoginViewModel: ObservableObject {
+    @Published var isLoggedIn: Bool = false
     
-    func kakaoLogin() {
-        // 카카오톡앱이 설치되어 있을 경우
-        if (UserApi.isKakaoTalkLoginAvailable()) {
-            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-                if let error = error {
-                    print(error)
-                }
-                else {
-                    print("loginWithKakaoTalk() success.")
-                    
-                    // 성공 시 동작 구현
-                    _ = oauthToken
-                }
-            }
-        } else { // 카카오톡앱이 설치되어 있지 않을 경우
-            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-                if let error = error {
-                    print(error)
-                }
-                else {
-                    print("loginWithKakaoAccount() success.")
-                    
-                    // 성공 시 동작 구현
-                    _ = oauthToken
-                }
+    func handleKakaoLogin() {
+        Task {
+            // 카카오톡 설치되어 있을 떄
+            if (UserApi.isKakaoTalkLoginAvailable()) {
+                // 카카오톡 앱으로 로그인
+                isLoggedIn = await handleLoginWithKakaoTalkApp()
+            } else { // 카카오톡 설치 안 되어 있을 때
+                // 카카오톡 계정으로 로그인
+                isLoggedIn = await handleLoginWithKakaoTalkAccount()
             }
         }
     }
     
     func kakaoLogout() {
-        UserApi.shared.logout {(error) in
-            if let error = error {
-                print(error)
+        Task {
+            if await handleKakaoLogout() {
+                isLoggedIn = false
             }
-            else {
-                print("logout() success.")
+        }
+    }
+    
+    func handleLoginWithKakaoTalkApp() async -> Bool {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                }
+                else {
+                    print("loginWithKakaoTalk() success.")
+                    // 성공 시 동작 구현
+                    _ = oauthToken
+                    continuation.resume(returning: true)
+                }
+            }
+        }
+    }
+    
+    func handleLoginWithKakaoTalkAccount() async -> Bool {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                }
+                else {
+                    print("loginWithKakaoAccount() success.")
+                    // 성공 시 동작 구현
+                    _ = oauthToken
+                    continuation.resume(returning: true)
+                }
+            }
+        }
+    }
+    
+    func handleKakaoLogout() async -> Bool {
+        await withCheckedContinuation { continuation in
+            UserApi.shared.logout {(error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                }
+                else {
+                    print("logout() success.")
+                    continuation.resume(returning: true)
+                }
             }
         }
     }
